f8dd33b1b895bab9009a32c6579e326df84ad587
Pablo Firulo <pfirulo@yahoo.com>
2024-01-22 18:03:42 -0300

    gorm agregado

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM golang:1.21.6
 
 WORKDIR /usr/src/app
+
 RUN go install github.com/cosmtrek/air@latest
+
 COPY . .
 RUN go mod tidy
--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+		return c.SendString("Hola Bruno y Franco👋!")
 	})
 
 	app.Listen(":3000")
